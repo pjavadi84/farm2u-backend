dc79b7849eb137e423b350e50633bc5e451421be
github username <github email address>
2020-06-02 22:12:45 -0700

    association between vendor and visitor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,5 @@
 class Vendor < ApplicationRecord
-    has_many :products 
+    has_many :products
+    belongs_to :event
+    has_many :visitors, through: :events
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,4 +1,5 @@
 class Visitor < ApplicationRecord
     belongs_to :event 
     has_many :comments
+    has_many :vendors, through: :event
 end
